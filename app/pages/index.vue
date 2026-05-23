cc54749e77b20db2e6a27090ba281926e27fa4bb
josh-dimpas <jk.dimpas@academy.opswerks.com>
2026-05-23 13:39:35 +0800

    :heavy_plus_sign: setup NuxtUI

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -3,7 +3,10 @@ const { x, y } = useMouse()
 </script>
 
 <template>
-    <div>This is index {{ x }} {{ y }}</div>
+    <div>
+        <div>This is index {{ x }} {{ y }}</div>
+        <UButton label="Test" />
+    </div>
 </template>
 
 <style lang="css" scoped></style>
